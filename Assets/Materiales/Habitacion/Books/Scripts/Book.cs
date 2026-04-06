ac43c73149bab7650a9776234ae39a3dcff2d06b
AstridCMelo <est.astridc.melo@unimilitar.edu.cogit config --global user.email>
2026-04-06 11:12:48 -0500

    Integración minijuego en misma escena

--- Assets/Materiales/Habitacion/Books/Scripts/Book.cs
+++ Assets/Materiales/Habitacion/Books/Scripts/Book.cs
@@ -5,15 +5,15 @@ public class Book : MonoBehaviour
 {
     private string titulo;
     private TextMeshProUGUI textTitle;
-    private char initialLetter => titulo[0];
+    private char initialLetter;
     void Start()
     {
         textTitle = GetComponentInChildren<TextMeshProUGUI>();
         titulo = textTitle.text;
+        initialLetter = titulo[0];
     }
     public char GetInitialLetter ()
     { 
         return initialLetter; 
     }
-
 }
