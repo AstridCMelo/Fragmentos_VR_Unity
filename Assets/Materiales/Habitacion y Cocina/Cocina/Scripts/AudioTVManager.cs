4bb034da02e96c500094a50ff57fb8caf97fa070
AstridCMelo <est.astridc.melo@unimilitar.edu.cogit config --global user.email>
2026-05-03 14:52:07 -0500

    Plugin de Meta para la espacializacion del sonido, estatica y sonido perilla en tv

---
+++ Assets/Materiales/Habitacion y Cocina/Cocina/Scripts/AudioTVManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioTVManager : MonoBehaviour
+{
+    public float inicio = 0f;
+    public AudioSource audioSource;
+    public bool PlaySound;
+    public bool StopSound;
+    public float fadeInTime = 2f;
+    public bool fadethatIn;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Referencia al audiosource
+        audioSource = GetComponent<AudioSource>();
+        //audioSource.PlayDelayed(0.2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlaySound)
+        {
+            RandomizeBeforePlaying();
+            PlaySound = false;
+        }
+        else if (StopSound)
+        {
+            StopPlaying();
+            StopSound = false;
+        }
+        else if (fadethatIn)
+        {
+            StartCoroutine(FadeIn());
+            fadethatIn = false;
+        }
+    }
+
+    //Cambios aleatorios en el sonido para evitar que se canse el oido
+    void RandomizeBeforePlaying()
+    {
+        audioSource.pitch = Random.Range(.97f, 1.03f);
+        audioSource.Play();
+    }
+
+    void StopPlaying()
+    {
+        audioSource.Stop();
+    }
+
+    //Corutina para FadeIn y FadeOut
+    IEnumerator FadeIn()
+    {
+        for (decimal i = 0m; i <= 1; i += 0.1m)
+        {
+            audioSource.volume = (float)i;
+            yield return new WaitForSeconds(fadeInTime / 100);
+        }
+    }
+}
--- Assets/Materiales/Habitacion y Cocina/Cocina/Scripts/MinigameDialControl.cs
+++ Assets/Materiales/Habitacion y Cocina/Cocina/Scripts/MinigameDialControl.cs
@@ -1,11 +1,22 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class MinigameDialControl : MonoBehaviour, IChangeChannel
-{ 
+{
+    private AudioTVManager audioManager;
     [SerializeField] float RightChannel = 270; //Grados que debe avanzar
     public UIController panel;
+   // [SerializeField] 
+
+    void Start()
+    {
+        audioManager = GetComponent<AudioTVManager>();
+    }
+
     public void ChannelChanged(float dialvalue)
     {
+        audioManager.PlaySound = true;
+
         Debug.Log("Channel " + dialvalue);
 
         if(dialvalue == RightChannel)
