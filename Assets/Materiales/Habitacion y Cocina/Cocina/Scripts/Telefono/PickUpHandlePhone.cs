cb57e014958928f04368c91f8c695aadfadd2af6
AstridCMelo <est.astridc.melo@unimilitar.edu.cogit config --global user.email>
2026-05-28 17:23:55 -0500

    agregar telefono arriba para marcar numero

--- Assets/Materiales/Habitacion y Cocina/Cocina/Scripts/Telefono/PickUpHandlePhone.cs
+++ Assets/Materiales/Habitacion y Cocina/Cocina/Scripts/Telefono/PickUpHandlePhone.cs
@@ -3,6 +3,7 @@ using UnityEngine.Rendering;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
+using static UnityEngine.Rendering.GPUSort;
 public class PickUpHandlePhone : MonoBehaviour
 {
     [SerializeField] AudioSource telephoneAudioSource;
@@ -14,11 +15,15 @@ public class PickUpHandlePhone : MonoBehaviour
     [SerializeField] float phoneTuneStart;
     private bool isGrabbed = false;
 
+    [SerializeField] Transform fixedPositionGrab;
+    [SerializeField] Transform fixPosition;
+    [SerializeField] XRSocketInteractor socketInteractor;
+
+    [SerializeField] float strenghtReturn = 20f;
+
     public UIController numberPanelController;
     public MiniGamePhone MiniGamePhone;
-    [SerializeField] Collider wireArea;
-    private Rigidbody rb;
-    private XRGrabInteractable HandleSelected => GetComponent<XRGrabInteractable>(); 
+    private IXRSelectInteractable HandleSelected => GetComponent<XRGrabInteractable>(); 
 
     private void OnEnable()
     {
@@ -37,27 +42,32 @@ public class PickUpHandlePhone : MonoBehaviour
         //Sonido hangup
         //isGrabbed=false;
         //Sound(hangUp, false, hangUpStart);
-    }
 
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody>();
+        IXRSelectInteractor interactor = arg0.interactorObject;
+
+        Debug.Log("Here handle");
+        Debug.Log(interactor);
+
+        if ((interactor is not XRSocketInteractor))
+        {
+            ReleasePhone();
+
+        }
     }
 
     private void Update()
     {
-        VerifyPosition();
         if(isGrabbed == true)
         {
             StayGrabbed();
         }
     }
 
-    private void Sound(AudioClip soundPhone, bool loop, float start)
+    private void Sound(AudioClip soundPhone, bool loop, float start, float pitch)
     {
         telephoneAudioSource.clip = soundPhone;
         telephoneAudioSource.time = start;
-        telephoneAudioSource.pitch = Random.Range(0.8f, 1.2f);
+        telephoneAudioSource.pitch = pitch;
         telephoneAudioSource.loop = loop;
         telephoneAudioSource.Play();
     }
@@ -69,8 +79,11 @@ public class PickUpHandlePhone : MonoBehaviour
 
         if((interactor is not XRSocketInteractor) && isGrabbed == false)
         {
+            FixedPositionGrab();
             //Sonido pickup
-            Sound(PickUp, false, PickUpStart);
+            telephoneAudioSource.volume = 1f;
+            float pitch = Random.Range(0.9f, 1.1f);
+            Sound(PickUp, false, PickUpStart,pitch);
             isGrabbed = true;
             Debug.Log("isGrabbed");
             numberPanelController.ShowImage();
@@ -79,11 +92,14 @@ public class PickUpHandlePhone : MonoBehaviour
         {
             if(isGrabbed == true)
             {
-                isGrabbed = false;
-                Sound(hangUp, false, hangUpStart);
+                ReleasePhone();
+                telephoneAudioSource.volume = 1f;
+                float pitch = Random.Range(0.9f, 1.1f);
+                Sound(hangUp, false, hangUpStart,pitch);
                 //Revisar si ya se coloco la fecha
                 //Borrar números
                 MiniGamePhone.EraseNumbers();
+                isGrabbed = false;
             }
         }
     }
@@ -93,27 +109,23 @@ public class PickUpHandlePhone : MonoBehaviour
         //Sonido tono
         if (!telephoneAudioSource.isPlaying)
         {
-            Sound(phoneTune, true, phoneTuneStart);
+            float pitch = 1.0f;
+            telephoneAudioSource.volume = 0.5f;
+            Sound(phoneTune, true, phoneTuneStart, pitch);
         }
     }
 
-    private void VerifyPosition()
+    public void FixedPositionGrab()
     {
-        float maxRadioWire = 2f;
+        HandleSelected.transform.localRotation = fixedPositionGrab.localRotation;
+        HandleSelected.transform.localPosition = fixedPositionGrab.localPosition;
+    }
 
-        if(transform.position.x > maxRadioWire)
-        {
-            //transform.position.x = maxRadioWire;
-            //rb.AddForce()
-
-
-        }else if(transform.position.y > maxRadioWire)
-        {
-
-        }else if (transform.position.z > maxRadioWire)
-        {
-
-        }
+    public void ReleasePhone()
+    {
+        HandleSelected.transform.localRotation = fixPosition.localRotation;
+        HandleSelected.transform.localPosition = fixPosition.localPosition;
+        socketInteractor.interactionManager.SelectEnter(socketInteractor, HandleSelected);
     }
 
 }
